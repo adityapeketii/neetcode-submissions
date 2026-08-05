0d91c521988949634e0e6a8d31da6589ad8295a4
Aditya Peketi <adityapeketi10@gmail.com>
2026-08-06 00:53:11 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        int n = s.length();
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            char ch = s[i];
+            if (ch == '(' || ch == '[' || ch == '{')
+            {
+                st.push(ch);
+            }
+            else {
+                if(ch == ')')
+                {
+                    if(st.empty() == false && st.top() == '(')
+                    {
+                        st.pop();
+                    }
+                    else return false;
+                }
+                else if(ch == ']')
+                {
+                    if(st.empty() == false && st.top() == '[')
+                    {
+                        st.pop();
+                    }
+                    else return false;
+                }
+                else if(ch == '}')
+                {
+                    if(st.empty() == false && st.top() == '{')
+                    {
+                        st.pop();
+                    }
+                    else return false;
+                }
+
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        else return false;
+    }
+};
