48c344d11c3c93a9176b7b48ec38f31e94c78975
Aditya Peketi <adityapeketi10@gmail.com>
2026-08-15 10:50:10 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,21 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def k_works(k):
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / k)
+
+            return hours <= h
+
+        low = 1
+        high = max(piles)
+
+        while low < high:
+            mid = (low + high)//2
+            if k_works(mid):
+                high = mid
+            else:
+                low = mid + 1
+
+        return low
